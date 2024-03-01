cc25faae17002bd6e7df669f32ed35f41911a9bd
Fernando Cornejo <ferreyes337@gmail.com>
2024-03-01 16:47:25 -0600

    feat: added color to status bar

--- src/app/components/TopBar/TopBar.tsx
+++ src/app/components/TopBar/TopBar.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading } from '@gluestack-ui/themed';
+import { Box, Heading, StatusBar, useToken } from '@gluestack-ui/themed';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
@@ -6,6 +6,7 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 export default function TopBar() {
   const [name, setName] = useState('');
   const inset = useSafeAreaInsets();
+  const statusBarBackground = useToken('colors', 'purple300');
 
   const getName = async () => {
     const userName = await AsyncStorage.getItem('userName');
@@ -17,16 +18,22 @@ export default function TopBar() {
   }, []);
 
   return (
-    <Box
-      bg="$purple300"
-      elevation="$4"
-      hardShadow="5"
-      pt={inset.top}
-      px="$4"
-      pb="$2">
-      <Heading fontSize="$xl" color="$textDark900" testID="topbar-greetings">
-        Hola, {name}
-      </Heading>
-    </Box>
+    <>
+      <StatusBar
+        backgroundColor={statusBarBackground}
+        barStyle="dark-content"
+      />
+      <Box
+        bg="$purple300"
+        elevation="$4"
+        hardShadow="5"
+        pt={inset.top}
+        px="$4"
+        pb="$2">
+        <Heading fontSize="$xl" color="$textDark900" testID="topbar-greetings">
+          Hola, {name}
+        </Heading>
+      </Box>
+    </>
   );
 }
